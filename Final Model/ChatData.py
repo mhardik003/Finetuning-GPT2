03d45c3758b437ae15822b9d453f8f1b30cdd533
Hardik Mittal <mhardik2003@gmail.com>
2023-04-18 09:29:29 +0530

    Copied the code from Ada

--- Final Model/ChatData.py
+++ Final Model/ChatData.py
@@ -1,71 +1,54 @@
 from torch.utils.data import Dataset
 import json
 
+
 class ChatData(Dataset):
-    def __init__(self, path:str, tokenizer):
+    def __init__(self, path: str, tokenizer):
 
         self.data = json.load(open(path, "r"))
 
         self.X = []
         for i in self.data:
-            for j in i['dialog']: 
+            for j in i['dialog']:
                 self.X.append(j['text'])
 
+        X_new = []
 
-        X_new=[]
-        
-        for idx in range(0,len(self.X),2):
+        for idx in range(0, len(self.X), 2):
             try:
                 i = self.X[idx]
+
                 if((len(self.X[idx].split(' ')) + len(self.X[idx+1].split(' '))) > 100):
-                        
-                        to_be_truncated = len(self.X[idx].split(' ')) + len(self.X[idx+1].split(' ')) - 100
-                        # print(to_be_truncated)
+                    
+                    to_be_truncated = len(self.X[idx].split(' ')) + len(self.X[idx+1].split(' ')) - 100
+                    self.X[idx] = " ".join(
+                        self.X[idx].split()[:-to_be_truncated])
+                    i = self.X[idx]
 
-                        # if(idx>=10 and idx<20):
-                        #     print(self.X[idx])
-                        #     print("-"*20)
-                        #     print(" ".join(self.X[idx].split(' ')[:-to_be_truncated]))
-                        self.X[idx] = " ".join(self.X[idx].split(' ')[:-to_be_truncated])
-                        
-                        # print(self.X[idx])
-                        # print("-"*20)
-                        i = self.X[idx]
-                        
-                string_feed= "<START> "+i+"\n<bot>: "+self.X[idx+1]+" <END>"
-                
-                if(len(string_feed.split(" "))>=300):
-                    print(idx)
-                    print(string_feed)
-                    print("----------------------------------------------------------------------------------------\n\n")
-
+                string_feed = "<START> " + \
+                    i + " <bot>: " + \
+                    " ".join(self.X[idx+1].split()) + " <END>"
 
                 X_new.append(string_feed)
-                
-                # if(idx>=0 and idx<20):
-                #     print("----------------------------------------------------------------------------------------")
-                #     print(self.X[idx])
-                #     print(X_new[-1])
-                                            
+
             except:
+                print("Error at index: "+str(idx))
                 break
+            
+
+        self.X = X_new
+        print(len(self.X))
+
+        self.X = self.X[:6000]
         
-        # print(len(self.X))
-        self.X=X_new
-
-        for x in range(0,20):
-            print(self.X[x])
-            print("-"*20)
-
-        self.X = self.X[:2000]
-        maxi = 0 
-        for x in self.X:
-            maxi = max(maxi, len(tokenizer(x)['input_ids']))
-        print("asdfasdfasd"+str(maxi)+"\n\n\n") 
         
-        # print(self.X[0])
+        # maxi = 0
+        # for x in self.X:
+        #     maxi = max(maxi, len(tokenizer(x)['input_ids']))
+        # print("Max length  : "+str(maxi)+"\n\n\n")
 
-        self.X_encoded = tokenizer(self.X,max_length=300, truncation=True, padding="max_length", return_tensors="pt")
+        self.X_encoded = tokenizer(
+            self.X, max_length=100, truncation=True, padding="max_length", return_tensors="pt")
         self.input_ids = self.X_encoded['input_ids']
         self.attention_mask = self.X_encoded['attention_mask']
 
--- Final Model/main.py
+++ Final Model/main.py
@@ -6,8 +6,10 @@ from tqdm import tqdm
 import torch
 
 
+FILENAME = "./Dataset/sheldon_chats.json"
+BATCH_SIZE = 8
+LEARNING_RATE = 1e-4
 
-FILENAME = "./Dataset/sheldon_chats_bigger_context.json"
 
 def clean_output(text):
     text = text.replace("<START>", "Question :")
@@ -18,72 +20,66 @@ def clean_output(text):
 
 
 def train(chatData, model, optim, NUM_EPOCHS=10):
-    
-    for i in tqdm(range(NUM_EPOCHS)):
-        # model.train()
-        temp=infer("Hello, how are you?")
 
-        with open("output.txt", 'a') as f:
-            blabla = "Epoch " + str(i) + "\n" + str(temp) + "\n--------------------------------------------\n\n"
-            print(blabla)
-            f.write(blabla)
-            
-            
+    for i in tqdm(range(NUM_EPOCHS)):
+        model.train()
+
+        if (i > 0):
+            temp = infer("Hello, how are you?")
+            with open("output.txt", 'a') as f:
+                blabla = "Epoch " + \
+                    str(i) + "\n" + str(temp) + \
+                    "\n--------------------------------------------\n\n"
+                # print(blabla)
+                f.write(blabla)
+
         # change learning rate
-        # if (i % 3 == 0 and optim.param_groups[0]['lr'] > 1e-3):
-            # optim.param_groups[0]['lr'] /= 2
+        if (i % 3 == 0 and optim.param_groups[0]['lr'] > 1e-5):
+            optim.param_groups[0]['lr'] /= 2
+
         for X, a in tqdm(chatData):
             X = X.to(device)
             a = a.to(device)
             optim.zero_grad()
-            
-            if (i % 50 == 0):
-                print('-'*100)
-                print(tokenizer.decode(X[0]))
-            
-            
-            # calculate the sentence after <bot>: token
-            response = X[:, X[0].tolist().index(tokenizer("<bot>:").input_ids[0]):]
-            
-            # pad the response to the max length of the input
-            response = torch.nn.functional.pad(response, (0, X.shape[1]-response.shape[1]), value=tokenizer.pad_token_id)
-            
-            print(response.shape, X.shape)
-            # print decoded response
-            # # print(tokenizer.decode(X[0]))
-            # if (tokenizer.decode(X[0]).find("<bot>:") == -1):
-            #     print   (tokenizer.decode(X[0]))
-                
-            
-            
-            
-            # calculate the output of the model 
-            output = model(X, attention_mask=a, labels=response)
-            loss = output.loss
-            
-            # output = model(X, attention_mask=a, labels=X) 
-            # compare only the sentence after <bot>: token with the actual output to calculate the loss
 
-            
+            # calculate the loss of the model
+            loss = model(X, attention_mask=a, labels=X).loss
+
             loss.backward()
             optim.step()
-            
+
         torch.save(model.state_dict(), "model_state.pt")
+
+            
+        print("-"*100)
         print(infer("Hello, how are you?"))
+        print(infer("What is your name?"))
+        print(infer("Is your name Sheldon? Yes or No?"))
+            
+        with open("output.txt", 'a') as f:
+            
+            f.write("\n" + "-"*100 + "\n")
+            f.write(infer("Hello, how are you?") + "\n")
+            f.write(infer("What is your name?") + "\n")
+            f.write(infer("Is your name Sheldon? Yes or No?") + "\n")
 
 
-
-def infer(inp):
+def infer(inp, f=0):
     # model.eval()
-    inp = "<START> "+inp+" <bot>: "
+    inp = "<START> "+inp+"<bot>: "
     inp = tokenizer(inp, return_tensors="pt")
+
     X = inp["input_ids"].to(device)
     a = inp["attention_mask"].to(device)
+
     output = model.generate(X, attention_mask=a)
     output = tokenizer.decode(output[0])
+
+    if (f):
+        index = output.find("<bot>:")
+        output = output[index:]
     # model.train()
     return clean_output(output)
-
 
 
 device = "cuda" if torch.cuda.is_available(
@@ -94,19 +90,13 @@ NUM_EPOCHS = input("Enter number of epochs : ")
 
 model_type = input("Enter 1 for GPT2 and 2 for GPTNeo : ")
 if model_type == "1":
-    model = GPT2LMHeadModel.from_pretrained("gpt2")
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-    config = model.config
+    model = GPT2LMHeadModel.from_pretrained("gpt2-medium")
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2-medium")
+    # config = model.config
 
 else:
     model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
     tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
-    config =  GPTNeoConfig.from_pretrained("EleutherAI/gpt-neo-125M")
-    config.attention_layers = ["global"] * 12
-    config.temperature = 0.5 # creativity setting (1 means most creative/random)
-    config.use_cache = True
-    config.repetition_penalty = 0.75 # how much to penalize for repeating words
-    model.config = config
 
 
 tokenizer.add_special_tokens({"pad_token": "<pad>",
@@ -114,46 +104,25 @@ tokenizer.add_special_tokens({"pad_token": "<pad>",
                               "eos_token": "<END>"})
 tokenizer.add_tokens(["<bot>:"])
 
-
 model.resize_token_embeddings(len(tokenizer))
-
-
-
-
 model.config.pad_token_id = tokenizer.pad_token_id
 model.config.bos_token_id = tokenizer.bos_token_id
 model.config.eos_token_id = tokenizer.eos_token_id
 model.config.max_length = 20
-model.config.use_cache = True # for faster generation of text
+model.config.use_cache = True  # for faster generation of text
 # model.config.repetition_penalty = 0.75 # how much to penalize for repeating words
 # model.config.temperature = 0.35 # creativity setting (1 means most creative/random)
-model.config.max_new_tokens = 300
-
-
-
-# generation_config = GenerationConfig(
-#     max_new_tokens=300,
-#     max_length=20,
-#     pad_token_id=tokenizer.pad_token_id,
-#     bos_token_id=tokenizer.bos_token_id,
-#     eos_token_id=tokenizer.eos_token_id,
-#     use_cache=True,
-# )
-
-
+model.config.max_new_tokens = 100
+model.config.attention_layers = ["global"] * 12
 
 model = model.to(device)
 
-# print(tokenizer.decode(model.generate(**tokenizer("hey i was good at basketball but ",
-#                          return_tensors="pt"))[0]))
-
-
 chatData = ChatData(FILENAME, tokenizer)
-chatData = DataLoader(chatData, batch_size=2)
+chatData = DataLoader(chatData, batch_size=BATCH_SIZE)
 
 model.train()
 
-optim = Adam(model.parameters(), lr=1e-5)
+optim = Adam(model.parameters(), lr=LEARNING_RATE)
 
 print("training .... ")
 train(chatData, model, optim, int(NUM_EPOCHS))
@@ -161,4 +130,4 @@ train(chatData, model, optim, int(NUM_EPOCHS))
 print("infer from model : ")
 while True:
     inp = input("You :")
-    print(infer(inp))
+    print(infer(inp, 1))
